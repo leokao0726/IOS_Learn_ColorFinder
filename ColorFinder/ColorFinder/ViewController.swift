937cbbf3cb8d0eccd6019dd33a782cf40e338274
LeoKao <leokao0726@gmail.com>
2017-05-10 13:13:21 +0900

    finish design

--- ColorFinder/ColorFinder/ViewController.swift
+++ ColorFinder/ColorFinder/ViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var redValue = 255
+    var greenValue = 255
+    var blueValue = 255
 
+    @IBOutlet weak var redText: UILabel!
+    @IBOutlet weak var greenText: UILabel!
+    @IBOutlet weak var blueText: UILabel!
+    
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        redValue = Int(sender.value)
+        redText.text = "R: \(redValue)"
+    }
+    
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        greenValue = Int(sender.value)
+        greenText.text = "G: \(greenValue)"
+    }
+    
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        blueValue = Int(sender.value)
+        blueText.text = "B: \(blueValue)"
+    }
+    
+    func changeBackgroundColor(){
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
